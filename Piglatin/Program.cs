948242ec79ea8ce40df12d8725b4cd423a2e663b
theadambyrne <adam.r.byrne@gmail.com>
2020-01-03 12:51:44 +0000

    Code from IDE

---
+++ Piglatin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Piglatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PIG LATIN \n This converts normal words into pig-latin ... enter a word below!");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+
+            var firstLetter = input[0];
+            string suffix = "ay";
+            string ending = firstLetter + suffix;
+            string result = input.Remove(0,1) + ending;
+            Console.WriteLine(result);
+
+        }
+    }
+}
